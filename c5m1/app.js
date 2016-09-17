8950ec6fded6a6e262d6291bc902b8a14d753770
mogen2014 <xiaojun0lee@gmail.com>
2016-09-17 12:24:06 +0800

    c5m1 some modification

---
+++ c5m1/app.js
@@ -0,0 +1,55 @@
+(function(){
+	'use strict';
+	angular.module('LunchCheck', [])
+	.controller('LunchCheckController', LunchCheckController);
+	LunchCheckController.$inject = ['$scope'];
+	function LunchCheckController ($scope){
+		$scope.items = "";
+		$scope.results = "";
+		var outputstyle = {};
+		var inputstyle = {};
+		$scope.checkLunch = function(){
+			if ($scope.items == "") {
+				outputstyle = {
+					"display": "inline-block",
+					"color": "red",
+				};
+				inputstyle = {
+						"border-color": "red",
+				};
+				$scope.results = "Please enter data first";
+			}else{
+				outputstyle = {
+						"display": "inline-block",
+						"color": "green",
+					};
+				inputstyle = {
+						"border-color": "green",
+				};
+
+				var initialItems = $scope.items.split(',');
+				var finalItmes = [];
+				var j = 0;
+				for (var i in initialItems){
+
+					initialItems[i] = initialItems[i].trim();
+					if (initialItems[i] != "") {
+						finalItmes[j] = initialItems[i];
+						j += 1;
+					}
+				}
+				// console.log(initialItems);
+				console.log(finalItmes);
+				if ( finalItmes.length > 3) {
+					$scope.results = "Too much!";
+				}else{
+					$scope.results = "Enjoy!";
+				}
+			}
+
+			$scope.outputstyle = outputstyle;
+			$scope.inputstyle = inputstyle;
+			return $scope.results;
+		};
+	};
+})();
