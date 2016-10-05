40c2b1d68da1dcdf902434f07d6b41598efb8ecd
mogen2014 <xiaojun0lee@gmail.com>
2016-10-05 21:55:39 +0800

    test master

--- c5m4/src/routes.js
+++ c5m4/src/routes.js
@@ -1,54 +1,42 @@
-(function(){
-	'use strict';
+(function () {
+'use strict';
 
-	angular.module('MenuApp')
-	.config(RoutesConfig);
+angular.module('ShoppingList')
+.config(RoutesConfig);
 
-	RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
-	function RoutesConfig($stateProvider, $urlRouterProvider){
-		$urlRouterProvider.otherwise('/');
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
 
-		$stateProvider
-		.state('home',{
-			url: '/',
-			templateUrl: 'src/templates/home.template.html'
-		})
-		.state('categories',{
-			url: '/categories',
-			templateUrl: 'src/templates/categories.template.html',
-			controller: 'CategoriesCtrl as categoriesCtrl',
-			resolve: {
-				categories: ['MenuDataService', function(MenuDataService){
-					return MenuDataService.getAllCategories();
-				}]
-			}
-		})
-		.state('itemDetail',{
-			url: '/itemDetail/{shortName}',
-			templateUrl: '/src/templates/item.template.html',
-			controller: 'ItemController as itemCtrl',
-			resolve: {
-				menuItems: ['$stateParams', 'MenuDataService', function($stateParams, MenuDataService){
-					return MenuDataService
-							.getItemsForCategoriy($stateParams.shortName)
-			   			  	.then(function(results){
-			   			  		return results;
-							});
-				}]
-			}
-		});
+  // Redirect to home page if no other URL matches
+  $urlRouterProvider.otherwise('/');
 
+  // *** Set up UI states ***
+  $stateProvider
 
+  // Home page
+  .state('home', {
+    url: '/',
+    templateUrl: 'src/shoppinglist/templates/home.template.html'
+  })
 
-		// .state('categories',{
-		// 	url: '/categories',
-		// 	templateUrl: 'src/templates/categories.template.html',
-		// 	controller: 'CategoriesCtrl as categoriesCtrl'
-		// })
-		// .state('itemDetail',{
-		// 	url: '/itemDetail/{shortName}',
-		// 	templateUrl: '/src/templates/item.template.html',
-		// 	controller: 'ItemController as itemCtrl'
-		// });
-	}
+  // Premade list page
+  .state('mainList', {
+    url: '/main-list',
+    templateUrl: 'src/shoppinglist/templates/main-shoppinglist.template.html',
+    controller: 'MainShoppingListController as mainList',
+    resolve: {
+      items: ['ShoppingListService', function (ShoppingListService) {
+        return ShoppingListService.getItems();
+      }]
+    }
+  })
+
+  .state('mainList.itemDetail', {
+    url: '/item-detail/{itemId}',
+    templateUrl: 'src/shoppinglist/templates/item-detail.template.html',
+    controller: "ItemDetailController as itemDetail"
+  });
+
+}
+
 })();
